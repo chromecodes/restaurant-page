706de64cc12c24e4043ad2043cd851af02c630c7
chromecodes <hameedhussain782@gmail.com>
2022-08-07 23:59:38 +0530

    updated home files

--- src/head.js
+++ src/head.js
@@ -1,44 +1,84 @@
- export const createHead = () => {
+export const createHead = () => {
 
-
-
-    const header = document.createElement("div");
-    header.classList.add("header");
+  const header = document.createElement("div");
+  header.classList.add("header");
 
     const name =  document.createElement('div');
     name.classList.add("name");
     name.textContent = "ICHIRAKU";
 
-    header.appendChild(name);
+  header.appendChild(name);
 
     const nav = document.createElement('div');
     nav.classList.add("nav");
 
-    const home = document.createElement('button');
-    home.classList.add("home");
-    home.textContent = "Home";
+      const homeCnt = document.createElement('div');
+      homeCnt.classList.add("homeCnt");
 
-    nav.appendChild(home);
+        const home = document.createElement('button');
+        home.classList.add("home");
+        home.textContent = "Home";
+    
+      homeCnt.appendChild(home);
 
-    const menu = document.createElement('button');
-    menu.classList.add("menu");
-    menu.textContent = "menu";
+        const span1 = document.createElement('div');
+        span1.classList.add("active-span1");
 
-    nav.appendChild(menu);
+        homeCnt.appendChild(span1);
 
-    const reservation = document.createElement('button');
-    reservation.classList.add("reservation");
-    reservation.textContent = "reservation";
+    nav.appendChild(homeCnt);
 
-    nav.appendChild(reservation)
 
-    const contact = document.createElement('button');
-    contact.classList.add("contact");
-    contact.textContent = "contact";
+      const menuCnt = document.createElement('div');
+      menuCnt.classList.add("menuCnt");
 
-    nav.appendChild(contact);
+        const menu = document.createElement('button');
+        menu.classList.add("menu");
+        menu.textContent = "menu";
 
-    header.appendChild(nav);
+      menuCnt.appendChild(menu);
 
-   main.appendChild(header);
- }
+        const span2 = document.createElement('div');
+        span2.classList.add("active-span2");
+
+      menuCnt.appendChild(span2);
+
+    nav.appendChild(menuCnt);
+
+      const reservationCnt = document.createElement('div');
+      reservationCnt.classList.add("reservationCnt");
+
+        const reservation = document.createElement('button');
+        reservation.classList.add("reservation");
+        reservation.textContent = "reservation";
+
+      reservationCnt.appendChild(reservation);
+
+        const span3 = document.createElement('div');
+        span3.classList.add("active-span3");
+
+      reservationCnt.appendChild(span3);
+
+    nav.appendChild(reservationCnt);
+   
+
+      const contactCnt = document.createElement('div');
+      contactCnt.classList.add("contactCnt");
+
+        const contact = document.createElement('button');
+        contact.classList.add("contact");
+        contact.textContent = "contact";
+
+      contactCnt.appendChild(contact);
+
+        const span4 = document.createElement('div');
+        span4.classList.add("active-span4");
+
+      contactCnt.appendChild(span4);
+
+    nav.appendChild(contactCnt);
+  
+  header.appendChild(nav);
+
+main.appendChild(header);
+}
--- src/home.js
+++ src/home.js
@@ -1,190 +1,217 @@
-export const createHomePage = () =>{
+export const createHomePage = () => {
+  const main = document.getElementById("main");
 
-    const main =  document.getElementById("main");
+    const contant = document.createElement("contant");
+    contant.classList.add("contant");
 
-        const contant = document.createElement('contant');
-        contant.classList.add('contant');
+      const left = document.createElement("div");
+      left.classList.add("left");
 
+        const intro = document.createElement("div");
+        intro.classList.add("intro");
 
-            const left = document.createElement('div');
-            left.classList.add('left');
+          const line1 = document.createElement("div");
+          line1.classList.add("line1");
+          line1.textContent = "Discover your taste";
 
-                const intro = document.createElement('div');
-                intro.classList.add('intro');
-                     
-                    const line1 = document.createElement('div');
-                    line1.classList.add('line1');
-                    line1.textContent = "Discover your taste"
+        intro.appendChild(line1);
 
-                intro.appendChild(line1);  
+          const line2 = document.createElement("div");
+          line2.classList.add("line2");
+          line2.textContent = "Eat tasty and healthy food";
 
+        intro.appendChild(line2);
 
+          const line3 = document.createElement("button");
+          line3.classList.add("line3");
+          line3.textContent = "Take a look";
 
+        intro.appendChild(line3);
 
+      left.appendChild(intro);
 
+        const homeIcons = document.createElement("div");
+        homeIcons.classList.add("home-icons");
 
+          const homeI1 = document.createElement("div")
+          homeI1.classList.add("home-icon-1");
 
-            left.appendChild(intro);  
-
-        contant.appendChild(left);
-
-            const right = document.createElement('div');
-            right.classList.add('right');
-
-                const radio1 = document.createElement('input');
-                radio1.setAttribute('type','radio');
-                radio1.setAttribute('name','radio-btn');
-                radio1.setAttribute('id','radio1');
-
-            right.appendChild(radio1);  
+        homeIcons.appendChild(homeI1);
         
-                const radio2 = document.createElement('input');
-                radio2.setAttribute('type','radio');
-                radio2.setAttribute('name','radio-btn');
-                radio2.setAttribute('id','radio2');
+          const homeI2 = document.createElement("div")
+          homeI2.classList.add("home-icon-2");
 
-            right.appendChild(radio2);  
+        homeIcons.appendChild(homeI2);
 
-                const radio3 = document.createElement('input');
-                radio3.setAttribute('type','radio');
-                radio3.setAttribute('name','radio-btn');
-                radio3.setAttribute('id','radio3');
+        const homeI3 = document.createElement("div")
+          homeI3.classList.add("home-icon-3");
 
-            right.appendChild(radio3);  
+        homeIcons.appendChild(homeI3);
 
-                const radio4 = document.createElement('input');
-                radio4.setAttribute('type','radio');
-                radio4.setAttribute('name','radio-btn');
-                radio4.setAttribute('id','radio4');
+        const homeI4 = document.createElement("div")
+          homeI4.classList.add("home-icon-4");
 
-            right.appendChild(radio4); 
-            
-                const radio5 = document.createElement('input');
-                radio5.setAttribute('type','radio');
-                radio5.setAttribute('name','radio-btn');
-                radio5.setAttribute('id','radio5');
+        homeIcons.appendChild(homeI4);
 
-            right.appendChild(radio5);  
-        
-                const radio6 = document.createElement('input');
-                radio6.setAttribute('type','radio');
-                radio6.setAttribute('name','radio-btn');
-                radio6.setAttribute('id','radio6');
+      left.appendChild(homeIcons);
 
-            right.appendChild(radio6);  
-        
-                const radio7 = document.createElement('input');
-                radio7.setAttribute('type','radio');
-                radio7.setAttribute('name','radio-btn');
-                radio7.setAttribute('id','radio7');
+    contant.appendChild(left);
 
-            right.appendChild(radio7);  
+      const right = document.createElement("div");
+      right.classList.add("right");
 
-                const slider = document.createElement('div');
-                slider.classList.add('slider');
+        const radio1 = document.createElement("input");
+        radio1.setAttribute("type", "radio");
+        radio1.setAttribute("name", "radio-btn");
+        radio1.setAttribute("id", "radio1");
 
-                    const label1 = document.createElement('label');
-                    label1.setAttribute('for','radio1');
-                    label1.setAttribute('class','slider-btn');
-                    label1.setAttribute('id','radio-1');
+      right.appendChild(radio1);
 
-                slider.appendChild(label1);  
+        const radio2 = document.createElement("input");
+        radio2.setAttribute("type", "radio");
+        radio2.setAttribute("name", "radio-btn");
+        radio2.setAttribute("id", "radio2");
 
-                    const label2 = document.createElement('label');
-                    label2.setAttribute('for','radio2');
-                    label2.setAttribute('class','slider-btn');
-                    label2.setAttribute('id','radio-2');
+      right.appendChild(radio2);
 
-                slider.appendChild(label2); 
+        const radio3 = document.createElement("input");
+        radio3.setAttribute("type", "radio");
+        radio3.setAttribute("name", "radio-btn");
+        radio3.setAttribute("id", "radio3");
 
-                    const label3 = document.createElement('label');
-                    label3.setAttribute('for','radio3');
-                    label3.setAttribute('class','slider-btn');
-                    label3.setAttribute('id','radio-3');
+      right.appendChild(radio3);
 
-                slider.appendChild(label3); 
+        const radio4 = document.createElement("input");
+        radio4.setAttribute("type", "radio");
+        radio4.setAttribute("name", "radio-btn");
+        radio4.setAttribute("id", "radio4");
 
-                    const label4 = document.createElement('label');
-                    label4.setAttribute('for','radio4');
-                    label4.setAttribute('class','slider-btn');
-                    label4.setAttribute('id','radio-4');
+      right.appendChild(radio4);
 
-                slider.appendChild(label4); 
+        const radio5 = document.createElement("input");
+        radio5.setAttribute("type", "radio");
+        radio5.setAttribute("name", "radio-btn");
+        radio5.setAttribute("id", "radio5");
 
-                    const label5 = document.createElement('label');
-                    label5.setAttribute('for','radio5');
-                    label5.setAttribute('class','slider-btn');
-                    label5.setAttribute('id','radio-5');
+      right.appendChild(radio5);
 
-                slider.appendChild(label5); 
+        const radio6 = document.createElement("input");
+        radio6.setAttribute("type", "radio");
+        radio6.setAttribute("name", "radio-btn");
+        radio6.setAttribute("id", "radio6");
 
-                    const label6 = document.createElement('label');
-                    label6.setAttribute('for','radio6');
-                    label6.setAttribute('class','slider-btn');
-                    label6.setAttribute('id','radio-6');
+      right.appendChild(radio6);
 
-                slider.appendChild(label6); 
+        const radio7 = document.createElement("input");
+        radio7.setAttribute("type", "radio");
+        radio7.setAttribute("name", "radio-btn");
+        radio7.setAttribute("id", "radio7");
 
-                    const label7 = document.createElement('label');
-                    label7.setAttribute('for','radio7');
-                    label7.setAttribute('class','slider-btn');
-                    label7.setAttribute('id','radio-7');
+      right.appendChild(radio7);
 
-                slider.appendChild(label7); 
+        const slider = document.createElement("div");
+        slider.classList.add("slider");
 
-            right.appendChild(slider); 
+          const label1 = document.createElement("label");
+          label1.setAttribute("for", "radio1");
+          label1.setAttribute("class", "slider-btn");
+          label1.setAttribute("id", "radio-1");
 
-                const imgs = document.createElement('div');
-                imgs.classList.add('imgs');
+        slider.appendChild(label1);
 
-                    const picture1 = document.createElement('img');
-                    picture1.classList.add('pic');
-                    picture1.classList.add('first');
-                    picture1.setAttribute('src','../src/img/p1.png');
+          const label2 = document.createElement("label");
+          label2.setAttribute("for", "radio2");
+          label2.setAttribute("class", "slider-btn");
+          label2.setAttribute("id", "radio-2");
 
-                imgs.appendChild(picture1);
+        slider.appendChild(label2);
 
-                    const picture2 = document.createElement('img');
-                    picture2.classList.add('pic');
-                    picture2.setAttribute('src','../src/img/p2.png');
+          const label3 = document.createElement("label");
+          label3.setAttribute("for", "radio3");
+          label3.setAttribute("class", "slider-btn");
+          label3.setAttribute("id", "radio-3");
 
-                imgs.appendChild(picture2);
+        slider.appendChild(label3);
 
-                    const picture3 = document.createElement('img');
-                    picture3.classList.add('pic');
-                    picture3.setAttribute('src','../src/img/p3.png');
+          const label4 = document.createElement("label");
+          label4.setAttribute("for", "radio4");
+          label4.setAttribute("class", "slider-btn");
+          label4.setAttribute("id", "radio-4");
 
-                imgs.appendChild(picture3);
+        slider.appendChild(label4);
 
-                    const picture4 = document.createElement('img');
-                    picture4.classList.add('pic');
-                    picture4.setAttribute('src','../src/img/p4.png');
+          const label5 = document.createElement("label");
+          label5.setAttribute("for", "radio5");
+          label5.setAttribute("class", "slider-btn");
+          label5.setAttribute("id", "radio-5");
 
-                imgs.appendChild(picture4);
+        slider.appendChild(label5);
 
-                    const picture5 = document.createElement('img');
-                    picture5.classList.add('pic');
-                    picture5.setAttribute('src','../src/img/p5.png');
+          const label6 = document.createElement("label");
+          label6.setAttribute("for", "radio6");
+          label6.setAttribute("class", "slider-btn");
+          label6.setAttribute("id", "radio-6");
 
-                imgs.appendChild(picture5);
+        slider.appendChild(label6);
 
-                    const picture6 = document.createElement('img');
-                    picture6.classList.add('pic');
-                    picture6.setAttribute('src','../src/img/p6.png');
+          const label7 = document.createElement("label");
+          label7.setAttribute("for", "radio7");
+          label7.setAttribute("class", "slider-btn");
+          label7.setAttribute("id", "radio-7");
 
-                imgs.appendChild(picture6);
+        slider.appendChild(label7);
 
-                    const picture7 = document.createElement('img');
-                    picture7.classList.add('pic');
-                    picture7.setAttribute('src','../src/img/p7.png');
+      right.appendChild(slider);
 
-                imgs.appendChild(picture7);
+        const imgs = document.createElement("div");
+        imgs.classList.add("imgs");
 
-            right.appendChild(imgs);
+          const picture1 = document.createElement("img");
+          picture1.classList.add("pic");
+          picture1.classList.add("first");
+          picture1.setAttribute("src", "../src/img/p1.png");
 
-        contant.appendChild(right);
+        imgs.appendChild(picture1);
 
-    main.appendChild(contant);
+          const picture2 = document.createElement("img");
+          picture2.classList.add("pic");
+          picture2.setAttribute("src", "../src/img/p2.png");
 
+        imgs.appendChild(picture2);
 
-}
+          const picture3 = document.createElement("img");
+          picture3.classList.add("pic");
+          picture3.setAttribute("src", "../src/img/p3.png");
+
+        imgs.appendChild(picture3);
+
+          const picture4 = document.createElement("img");
+          picture4.classList.add("pic");
+          picture4.setAttribute("src", "../src/img/p4.png");
+
+        imgs.appendChild(picture4);
+
+          const picture5 = document.createElement("img");
+          picture5.classList.add("pic");
+          picture5.setAttribute("src", "../src/img/p5.png");
+
+        imgs.appendChild(picture5);
+
+          const picture6 = document.createElement("img");
+          picture6.classList.add("pic");
+          picture6.setAttribute("src", "../src/img/p6.png");
+
+        imgs.appendChild(picture6);
+
+          const picture7 = document.createElement("img");
+          picture7.classList.add("pic");
+          picture7.setAttribute("src", "../src/img/p7.png");
+
+        imgs.appendChild(picture7);
+
+      right.appendChild(imgs);
+
+    contant.appendChild(right);
+
+  main.appendChild(contant);
+};
